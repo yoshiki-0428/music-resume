e0bcc08d5a54f88e662902fe40c4a6a5b95bc698
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2022-11-17 23:16:43 +0900

    [add]

--- src/pages/resume/new.tsx
+++ src/pages/resume/new.tsx
@@ -1,4 +1,5 @@
-import SurveyResume from "@/components/resume/survey/SurveyResume";
+import dynamic from "next/dynamic";
+const SurveyResume = dynamic(() => import('@/components/resume/survey/SurveyResume'), { ssr: false });
 
 export default function ResumeNew() {
   return (
