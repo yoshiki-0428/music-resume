21a90af52052f62e872b107889dbf7ff83b8b4b8
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2022-11-18 02:26:03 +0900

    filter

--- src/pages/api/tracks.ts
+++ src/pages/api/tracks.ts
@@ -5,7 +5,20 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import bump from './bbb.json'
 
 export default function tracks(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(bump.filter((record) => {
+  const json = bump.filter((record) => {
     return record.artists[0].name === 'BUMP OF CHICKEN'
-  }));
+  })
+
+  const hash: { [key: string]: any } = {}
+  json.forEach(record => {
+    if (hash[record.name]) {
+      if (hash[record.name].album.album_type === 'single' && record.album.album_type === 'album') {
+        hash[record.name] = record
+      }
+    } else {
+      hash[record.name] = record
+    }
+  })
+
+  res.status(200).json(Object.keys(hash).map(k => hash[k]));
 }
