e127eddd959137510918d170f998909588a48304
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2022-11-20 20:59:46 +0900

    add coffee button

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -22,6 +22,7 @@ export default function Header() {
                 </UnstyledLink>
               </li>
             ))}
+            <a href="https://www.buymeacoffee.com/yoshiki0428"><img className='h-10' src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=yoshiki0428&button_colour=FF5F5F&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00" /></a>
           </ul>
         </nav>
       </div>
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -15,4 +15,5 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       </UnderlineLink>
     </footer>
   </>;
+
 }
