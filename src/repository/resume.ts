180bac8718ae224ce90748dd83a1bccbe3d100a9
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2022-11-20 19:31:22 +0900

    fix error

--- src/repository/resume.ts
+++ src/repository/resume.ts
@@ -1,4 +1,3 @@
-import logger from "@/lib/logger";
 import {client} from "@/lib/redis";
 
 import {artistID} from "@/constant/env";
@@ -6,28 +5,19 @@ import {getTracks} from "@/repository/track";
 import {ResumeType} from "@/repository/types";
 
 export const getResume = async (id: string): Promise<ResumeType | null> => {
+  await client.connect()
+  const data = await client.get(`${artistID}:resume:${id}`)
+  await client.disconnect()
+  if (data) {
+    const resume: ResumeType = JSON.parse(data)
+    resume.tracks = []
+    const tracks = await getTracks(artistID)
+    resume.songs.forEach(song => {
+      const track = tracks?.find(track => track.id === song)
+      if (track) resume.tracks.push(track)
+    })
 
-  try {
-    await client.connect()
-    const data = await client.get(`${artistID}:resume:${id}`)
-    await client.disconnect()
-    if (data) {
-      const resume: ResumeType = JSON.parse(data)
-      resume.tracks = []
-      const tracks = await getTracks(artistID)
-      resume.songs.forEach(song => {
-        const track = tracks?.find(track => track.id === song)
-        if (track) resume.tracks.push(track)
-      })
-      resume.joining?.forEach(join => {
-        // TODO: getJoining & setJoinningObject
-      })
-
-      return resume
-    }
-  } catch (e) {
-    logger(e)
-  } finally {
+    return resume
   }
   return null
 }
